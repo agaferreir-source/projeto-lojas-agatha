7664c48bbfdb22d0e289ee54bf154a2be9d8de1a
agaferreir-source <agaferreir@gmail.com>
2026-09-15 12:46:31 -0300

    Modify project metadata in layout.tsx
    Updated metadata title and description for the project.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,30 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Projeto Lojas | Agatha Pinheiro",
+  title: "Participe do Projeto | Agatha Pinheiro",
   description:
-    "Uma iniciativa para desenvolver soluções digitais para negócios reais.",
+    "Inscreva sua empresa para participar de um projeto de desenvolvimento de uma solução digital personalizada.",
+  keywords: [
+    "Agatha Pinheiro",
+    "projeto",
+    "desenvolvimento",
+    "tecnologia",
+    "sistema",
+    "site",
+    "automação",
+    "solução digital",
+  ],
+  authors: [
+    {
+      name: "Agatha Pinheiro",
+    },
+  ],
+  openGraph: {
+    title: "Participe do Projeto | Agatha Pinheiro",
+    description:
+      "Uma oportunidade para negócios reais desenvolverem uma solução digital personalizada.",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
